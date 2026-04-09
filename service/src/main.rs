a15d17bc52dc03848b96a7888e7a664ebcc0b956
pegn0ly <owafe001@gmail.com>
2026-04-09 17:53:03 +0300

    setup finished(2)

--- service/src/main.rs
+++ service/src/main.rs
@@ -1,4 +1,4 @@
-// use migration::MigratorTrait;
+use migration::MigratorTrait;
 use shared_gen::auth::auth_service_server::AuthServiceServer;
 use crate::auth::AuthServiceImpl;
 use crate::error::ServiceError;
@@ -12,7 +12,7 @@ async fn main() -> Result<(), ServiceError> {
     dotenv::dotenv().ok();
     let addr = format!("[::1]:{}", std::env::var("PORT")?).parse().unwrap();
     let db_connection = sea_orm::Database::connect(std::env::var("DB_URL")?).await?;
-    // migration::Migrator::up(&db_connection, None).await?;
+    migration::Migrator::up(&db_connection, None).await?;
     let auth_service = AuthServiceImpl::new(db_connection);
 
     tonic::transport::Server::builder()
--- shared/src/lib.rs
+++
@@ -1,9 +0,0 @@
-pub mod auth;
-pub mod common;
-pub mod hero_type_enum;
-
-use tonic::include_proto;
-
-// include_proto!("auth");
-// include_proto!("hero_type_enum");
-// include_proto!("common");
